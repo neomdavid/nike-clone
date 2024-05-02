b58284467f1f9eb4cd70cb0bec87b615d4fa06da
neomdavid <neomanaogdavid@gmail.com>
2024-05-02 12:43:43 +0800

    Creates renderPaymentSummary but w/o proper functions and files separation

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,6 +1,7 @@
 import { cart, removeFromCart } from "./data/cart.js";
 import {getShoeByVariationId} from "./data/shoes.js";
-import { getVariationByVariationId} from "./data/variation.js"
+import { getVariationByVariationId} from "./data/variation.js";
+import { formatCurrency } from "./utils/formatCurrency.js";
 
 console.log(cart);
 function renderCartSummary(){
@@ -84,5 +85,52 @@ function sizesHTML(variation){
 }
 };
 
-
 renderCartSummary();
+
+
+function renderPaymentSummary(){
+  let cartSummaryHTML = ``;
+  let cartSubtotal = 0;
+  let deliveryFee = 0;
+  let total = 0;
+
+  cart.forEach((cartItem)=>{
+    const shoe = getShoeByVariationId(cartItem.variationId);
+    
+    const variation = getVariationByVariationId(cartItem.variationId);
+
+    cartSubtotal+=shoe.priceCents;
+
+  });
+
+  total = cartSubtotal + deliveryFee;
+
+  console.log(formatCurrency(cartSubtotal));
+  console.log(formatCurrency(total));
+
+  cartSummaryHTML+=`
+      <div class="summary-title">Summary</div>
+        <div class="subtotal-container">
+          <div class="subtotal-title">Subtotal</div>
+          <div class="subtotal-price">&#8369;${formatCurrency(cartSubtotal)}</div>
+        </div>
+        <div class="delivery-container">
+          <div class="delivery-title">Estimated Delivery & Handling</div>
+          <div class="delivery-value">Free</div>
+        </div>
+        <div class="total-container">
+          <div class="total-title">Total</div>
+          <div class="total-price">&#8369;${formatCurrency(total)}</div>
+        </div>
+        <div class="checkout-button">Guest Checkout</div>
+        <div class="checkout-button">Member Checkout</div>
+        <div>
+      </div>
+  `
+
+
+  document.querySelector('.summary-container')
+    .innerHTML = cartSummaryHTML;
+};
+
+renderPaymentSummary();
